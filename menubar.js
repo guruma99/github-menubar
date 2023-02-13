e140b121b49beb3fde1d7cb731177940b81d3763
깃허브아이디 <>
2023-02-13 15:11:14 +0900

    💄 바닐라js로 변경

--- menubar.js
+++ menubar.js
@@ -1,9 +1,15 @@
-$(".user").on("click", function(){
-  if($(".menu").hasClass('on')) {
-    $(".menu").removeClass('on');
-    $(".dropdown-caret").removeClass('on');
-  } else {
-    $(".menu").addClass('on');
-    $(".dropdown-caret").addClass('on');
-  }
+// $(".user").on("click", function(){
+//   if($(".menu").hasClass('on')) {
+//     $(".menu").removeClass('on');
+//     $(".dropdown-caret").removeClass('on');
+//   } else {
+//     $(".menu").addClass('on');
+//     $(".dropdown-caret").addClass('on');
+//   }
+// });
+
+document.querySelector(".user").addEventListener('click', function(){
+  document.querySelector(".menu").classList.toggle("on");
+  document.querySelector(".dropdown-caret").classList.toggle("on");
 });
+
